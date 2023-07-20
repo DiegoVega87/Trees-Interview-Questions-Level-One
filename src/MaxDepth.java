fd064ab350edb0ed697d8a5327224da15f53d17d
Diego Vega <diegovega@me.com>
2023-07-20 17:56:33 +0200

    Add examples

--- src/MaxDepth.java
+++ src/MaxDepth.java
@@ -8,6 +8,28 @@ public class MaxDepth {
     * nodes representing their subordinates. The output should be an integer representing the maximum depth of the
     * organizational tree.
     *
+    * Example 1:
     *
-    * */
+    *   TreeNode root = new TreeNode(1);
+    *   root.left = new TreeNode(2);
+    *   root.right = new TreeNode(3);
+    *   root.left.left = new TreeNode(4);
+    *   root.left.right = new TreeNode(5);
+    *   root.right.left = new TreeNode(6);
+    *   root.right.right = new TreeNode(7);
+    *   maxDepth = 3
+    *
+    *   Example 2:
+    *
+    *   TreeNode root = new TreeNode(1);
+    *   root.left = new TreeNode(2);
+    *   root.right = new TreeNode(3);
+    *   root.left.left = new TreeNode(4);
+    *   root.left.right = new TreeNode(5);
+    *   root.right.left = new TreeNode(6);
+    *   root.right.right = new TreeNode(7);
+    *   root.left.left.left = new TreeNode(8);
+    *   root.left.left.right = new TreeNode(9);
+    *   maxDepth = 4
+     * */
 }
