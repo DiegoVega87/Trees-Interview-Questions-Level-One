4fae15807b3eb906a6926db9059b71f090954d23
Diego Vega <diegovega@me.com>
2023-07-20 18:33:36 +0200

    Problem description

---
+++ src/IsValidBST.java
@@ -0,0 +1,14 @@
+public class IsValidBST {
+
+    /*
+    * As a startup inventory management system, we need to validate the binary search tree data structure used to
+    * store our inventory items. We have a root node of the binary tree which contains the item information such
+    * as item ID, name, quantity, and price. We want to make sure that the binary search tree is valid and follows the
+    * rules of a binary search tree where the left subtree of a node contains only nodes with keys less than the node's
+    * key and the right subtree of a node contains only nodes with keys greater than the node's key.
+    *
+    * Write a function that takes the root of the binary tree as input and returns a boolean value indicating whether
+    * the tree is a valid binary search tree or not. The function should handle the case where the binary tree is empty.
+    *
+     * */
+}
