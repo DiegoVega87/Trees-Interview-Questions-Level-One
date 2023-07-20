30b2d511506e218340451fa1378500fe026136ae
Diego Vega <diegovega@me.com>
2023-07-20 17:53:33 +0200

    Problem explanation

---
+++ src/MaxDepth.java
@@ -0,0 +1,13 @@
+public class MaxDepth {
+
+    /*
+    * As a fast-growing startup, our company has a complex organizational structure with multiple levels of hierarchy.
+    * We need to determine the maximum depth of our organizational tree to ensure efficient communication and
+    * decision-making processes. Write a function that takes in the root of our organizational tree and returns
+    * its maximum depth. The input will be a binary tree where each node represents an employee and has two children
+    * nodes representing their subordinates. The output should be an integer representing the maximum depth of the
+    * organizational tree.
+    *
+    *
+    * */
+}
