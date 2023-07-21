e6ee2fc952fd41968c69874551570c08ed1e7fd9
Diego Vega <diegovega@me.com>
2023-07-21 15:52:56 +0200

    Add problem explanation

---
+++ src/ValueTraversal.java
@@ -0,0 +1,11 @@
+public class ValueTraversal {
+
+    /*
+    * We need to develop a feature that displays a binary tree level traversal to help students visualize the hierarchy
+    * of a course curriculum. Given the root of a binary tree representing a course curriculum, implement a function
+    * that returns the level order traversal of its nodes' values. The function should return a list of lists,
+    * where each inner list represents a level of the binary tree and contains the values of the nodes at that
+    *  level from left to right.
+    *
+    * */
+}
