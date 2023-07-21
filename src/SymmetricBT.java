c138238ba40576b2f8d1fa9a759742481741c17a
Diego Vega <diegovega@me.com>
2023-07-21 12:03:08 +0200

    Add problem explanation

---
+++ src/SymmetricBT.java
@@ -0,0 +1,11 @@
+public class SymmetricBT {
+
+    /*
+    * As a startup in the education industry, we want to create an online platform to help students learn programming.
+    * We need a function that takes the root of a binary tree as input and checks whether the tree is a mirror of itself,
+    * i.e. symmetric around its center. This will help us ensure that our binary tree exercises are correctly labeled
+    * as symmetric or not. Can you implement a function that takes the root of a binary tree and returns true if the
+    * tree is symmetric, false otherwise?
+    *
+     * */
+}
