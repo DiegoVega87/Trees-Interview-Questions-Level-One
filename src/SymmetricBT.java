bb23f09925adebfab5877bbf3a3364f1c7f60cdd
Diego Vega <diegovega@me.com>
2023-07-21 15:50:03 +0200

    Add solution

--- src/SymmetricBT.java
+++ src/SymmetricBT.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class SymmetricBT {
 
     /*
@@ -7,5 +9,43 @@ public class SymmetricBT {
     * as symmetric or not. Can you implement a function that takes the root of a binary tree and returns true if the
     * tree is symmetric, false otherwise?
     *
-     * */
+    *
+    * Example 1:
+    *        1
+    *       / \
+    *      2   2
+    *     / \ / \
+    *    3  4 4  3
+    *   Output = true
+    *
+    * Example 2:
+    *       1
+    *      / \
+    *     2   2
+    *      \   \
+    *       3    3
+    *   Output: false
+    *
+    * */
+
+    public static boolean isSymmetric(TreeNode root){
+        if(root == null){
+            return true;
+        }
+        return isSymmetric(root.left,root.right);
+    }
+
+    private static boolean isSymmetric(TreeNode left, TreeNode right){
+
+        if(left == null && right == null){
+            return true;
+        }
+        if(left == null || right == null){
+            return false;
+        }
+        if(left.val != right.val){
+            return false;
+        }
+        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+    }
 }
