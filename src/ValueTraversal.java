f8b21194bd5ae9a51ebc3d1460a5d76a7da64d2f
Diego Vega <diegovega@me.com>
2023-07-22 21:09:59 +0200

    Add part of the solution

--- src/ValueTraversal.java
+++ src/ValueTraversal.java
@@ -1,3 +1,10 @@
+import java.lang.invoke.CallSite;
+import java.time.temporal.Temporal;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class ValueTraversal {
 
     /*
@@ -24,5 +31,32 @@ public class ValueTraversal {
     *            /     \
     *           4       5
     *
-     * */
+    * */
+
+    public static List<List<Integer>> levelOrderTraversal(TreeNode root){
+        if(root == null){
+            return null;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> list = new ArrayList<>();
+
+        queue.add(root);
+        list.add(new ArrayList<>(root.val));
+
+        while(!queue.isEmpty()){
+
+            TreeNode temp = queue.poll();
+            List<Integer> tempList = new ArrayList<>();
+
+            if(temp.left != null){
+                queue.add(temp.left);
+            }
+
+            if(temp.right != null){
+                queue.add(temp.right);
+            }
+
+        }
+
+    }
 }
